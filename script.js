85429a7a5a898e39acada56aa0501021ee80787b
faberlissss <maria.simochka@gmail.com>
2024-01-18 01:56:46 +0200

    fix2

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const array = [1, 2, 3, 4, 5, 6, 7];
 function removeElement(array, item) {
     let itemIndex = (element) => element === item;
     let findItemIndex = array.findIndex(itemIndex);
-    if (findItemIndex !== item) {
-        array.splice(findItemIndex, 1);
-    }
+    array.splice(findItemIndex, 1);
 }
 removeElement(array, 5);
 console.log(array);
